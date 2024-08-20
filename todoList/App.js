2cddf38d018ffe45b676db18db6814ae1ecd914d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-21 04:32:37 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- todoList/App.js
+++ todoList/App.js
@@ -4,8 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* today's tacks */}
+      <View style={styles.TaskWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          {/* Task on on here */}
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+
+  TaskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+
+  items: {
+
+  }
 });
---
+++ todoList/components/Task.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet} from 'react-native'
+
+const Task = () => {
+  return (
+    <View>
+        <Text> Task Text</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default Task
