8576f19279ecdea97a1592ae33ec99794f05f09d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-21 04:50:43 +0530

    Update Task.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- todoList/components/Task.js
+++ todoList/components/Task.js
@@ -5,7 +5,7 @@ const Task = (props) => {
   return (
     <View style={styles.item}>
         <View style={styles.itemLeft}>
-            <TouchableOpacity style={styles.square}></TouchableOpacity>
+            <View style={styles.square}></View>
             <Text style={styles.itemText}> {props.text} </Text>
         </View>
         <View style={styles.circular}></View>
@@ -15,19 +15,36 @@ const Task = (props) => {
 
 const styles = StyleSheet.create({
     item: {
-
+        backgroundColor: '#FFF',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginBottom: 20,        
     },
     itemLeft: {
-
+        flexDirection: 'row',
+        alignContent: 'center',
+        flexWrap: 'wrap'
     },
     square: {
-
+        width: 24,
+        height: 24,
+        backgroundColor: '#55BCF6',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15
     },
     itemText: {
-
+        maxWidth: '80%'
     },
     circular: {
-        
+        width: 12,
+        height: 12,
+        borderColor: '#55BCF6',
+        borderWidth: 2,
+        borderRadius: 5
     }
 })
 
